da50e86a37ef7d212cfba2a137eeef8c6518c3fc
Rory Becker <RoryBecker@gmail.com>
2014-05-02 20:43:28 +0100

    - Bumped version to 1.1.

--- CR_ConvertStringToAppSetting/Properties/AssemblyInfo.cs
+++ CR_ConvertStringToAppSetting/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using DevExpress.CodeRush.Common;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
